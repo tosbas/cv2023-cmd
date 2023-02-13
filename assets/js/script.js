05063b4fbec2990f3bcb7475c95fd79771d7df39
Revenge_Master <69684754+tosbas@users.noreply.github.com>
2023-02-13 20:30:22 +0100

    Add files via upload
    Modification style et ajout argument ls

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,14 +10,18 @@ const welcomeText =
 const arrayText = welcomeText.split(" ");
 const arrayString = welcomeText.split("");
 const NAME_SHELL = "tosbas shell 1.0.0";
-const HOST = "tosbas@localhost:~$";
+const HOST = "tosbas";
+const DOMAIN = "localhost";
 
 const commandesListe = [
-  "ls => Lister les répértoires",
-  "cat => Lire un fichier",
-  "cd => Changer de répertoire",
-  "clear => nettoyer le terminale",
+  "ls [-a]",
+  "cat",
+  "cd",
+  "clear",
+  "whoami",
 ];
+
+const directories = [["Documents", "Downloads"], [".hidden"]];
 
 //**** Affichage  ******/
 const welcomeMessage = setInterval(() => {
@@ -42,13 +46,11 @@ const term = (string, id) => {
   if (string !== undefined) {
     newMessageError(string);
     inputReadOnly(id);
-
     const newId = incInput(id);
-
     newShell(newId);
   } else {
     shellhost.forEach((element) => {
-      element.innerHTML += `${HOST}`;
+      element.innerHTML += `${HOST}@${DOMAIN}:~$`;
     });
 
     document.getElementById(promptInput.id).classList.remove("prompt-disabled");
@@ -61,7 +63,7 @@ const newShell = (newId) => {
   const newShellCmd = document.createElement("div");
   newShellCmd.classList.add("shell-cmd");
   shell.appendChild(newShellCmd);
-  newShellCmd.innerHTML = `<p class="shell-host">${HOST}</p>`;
+  newShellCmd.innerHTML = `<p class="shell-host">${HOST}@${DOMAIN}:~$</p>`;
 
   const newInput = document.createElement("input");
   newShellCmd.appendChild(newInput);
@@ -99,12 +101,45 @@ const help = (shellCmd, newId) => {
   });
 
   inputReadOnly(newId);
-  const getId = newId.split("prompt");
+  const newIdShell = incInput(newId);
+  newShell(newIdShell);
+};
 
-  let newIdShell = parseInt(getId[1]);
+const ls = (shellCmd, newId, arg) => {
+  const shellContext = document.getElementById(shellCmd);
+  const commande = document.createElement("div");
 
-  newIdShell++;
+  shellContext.appendChild(commande);
 
+  if (arg === undefined) {
+    directories[0].forEach((element) => {
+      commande.innerHTML += `<p>${element}</p>`;
+    });
+  } else {
+    switch (arg) {
+      case "-a":
+        directories[0].concat(directories[1]).forEach((element) => {
+          commande.innerHTML += `<p>${element}</p>`;
+        });
+        break;
+    }
+  }
+
+  inputReadOnly(newId);
+  const newIdShell = incInput(newId);
+  newShell(newIdShell);
+};
+
+const whoami = (shellCmd, newId) => {
+  const shellContext = document.getElementById(shellCmd);
+  const commande = document.createElement("div");
+
+  shellContext.appendChild(commande);
+
+  commande.innerHTML += `<p>${HOST}</p>`;
+
+  inputReadOnly(newId);
+  const newIdShell = incInput(newId);
   newShell(newIdShell);
 };
 
@@ -117,11 +152,8 @@ const inputReadOnly = (id) => {
 
 const incInput = (id) => {
   const getId = id.split("prompt");
-
   let newId = parseInt(getId[1]);
-
   newId++;
-
   return newId;
 };
 
@@ -129,10 +161,19 @@ const EventInput = (newId) => {
   const promptInput = document.getElementById(newId);
   promptInput.addEventListener("keypress", (e) => {
     const value = promptInput.value;
+    const valueArg = value.split(" ");
+
     if (e.key === "Enter") {
-      switch (value) {
+      switch (valueArg[0]) {
         case "ls":
-          console.log("ls");
+          switch (valueArg[1]) {
+            case "-a":
+              ls(shell.id, newId, "-a");
+              break;
+            default:
+              ls(shell.id, newId);
+              break;
+          }
           break;
         case "cat":
           console.log("cat");
@@ -145,6 +186,9 @@ const EventInput = (newId) => {
           break;
         case "help":
           help(shell.id, newId);
+          break;
+        case "whoami":
+          whoami(shell.id, newId);
           break;
         default:
           term("Commande non reconnu", newId);
